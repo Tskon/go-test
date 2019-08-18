5d24304fd99e7431c95e40a598b9a8396a858d04
tskon <tsekhmeister.k@gmail.com>
2019-08-18 21:42:21 +0300

    add gitignore

--- hello.go
+++ hello.go
@@ -1,14 +1,18 @@
 package main
 
 import (
-  "fmt"
-  "utils"
+	"fmt"
+	"utils"
 )
 
-func main()  {
-  fmt.Println("hello world")
-  fmt.Println(utils.Sum(1, 2))
-  fmt.Println(utils.Sum2)
-  fmt.Println(utils.A)
-  //fmt.Println(utils.b)
+func main() {
+	slice := make([]int, 4, 7)
+	slice2 := []int{2, 0, 5, 4}
+	slice2 = append(slice2, 3, 2)
+
+	fmt.Println("hello world")
+	fmt.Println(utils.Sum(1, 2))
+	fmt.Println(utils.Sum2)
+	fmt.Println(utils.A)
+	fmt.Println(slice, cap(slice2))
 }
