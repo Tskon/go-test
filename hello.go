e6ddb4b7d39d51a1fca9e90030ec2c9df02ad3b9
tskon <tsekhmeister.k@gmail.com>
2019-08-18 21:43:33 +0300

    append test

--- hello.go
+++ hello.go
@@ -8,11 +8,11 @@ import (
 func main() {
 	slice := make([]int, 4, 7)
 	slice2 := []int{2, 0, 5, 4}
-	slice2 = append(slice2, 3, 2)
+	slice2 = append(slice2, 3, 2, 4, 5, 6)
 
 	fmt.Println("hello world")
 	fmt.Println(utils.Sum(1, 2))
 	fmt.Println(utils.Sum2)
 	fmt.Println(utils.A)
-	fmt.Println(slice, cap(slice2))
+	fmt.Println(slice, cap(slice2), slice2)
 }
