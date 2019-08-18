17a200032ad9419bc77fd5b1f31f8d1b97c8a475
tskon <tsekhmeister.k@gmail.com>
2019-08-18 21:31:39 +0300

    Initial commit

---
+++ hello.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+  "fmt"
+  "utils"
+)
+
+func main()  {
+  fmt.Println("hello world")
+  fmt.Println(utils.Sum(1, 2))
+  fmt.Println(utils.Sum2)
+  fmt.Println(utils.A)
+  //fmt.Println(utils.b)
+}
---
+++ src/utils/utils.go
@@ -0,0 +1,9 @@
+package utils
+
+func Sum(a, b int) int {
+  return a + b
+}
+
+var A, b = 3, 4
+
+var Sum2 = Sum(A, b)
